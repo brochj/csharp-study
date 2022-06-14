6b808f724146a5dda37f41feacb120bdb42df7a6
brochj <brochj@gmail.com>
2022-06-14 11:07:02 -0300

    backup 06/14/2022

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -272,11 +272,11 @@ namespace Codewars
             // [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
 
             bool isOdd(int num) => num % 2 != 0;
-            int[] odds;
-            foreach (var item in array)
-            {
-                List.
-            }
+            // int[] odds;
+            // foreach (var item in array)
+            // {
+            //     List.
+            // }
             return array;
         }
 
---
+++ Functions/Program.cs
@@ -0,0 +1,15 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, };
+
+        Console.WriteLine(ArrayToString(numbers));
+
+    }
+    public static string ArrayToString(int[] array)
+    {
+        return String.Join('-', array);
+    }
+}
